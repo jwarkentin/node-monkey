jwarkentin/node-monkey

commit 0bc8402947150c5628d457b5f089234de6d85e30

Finish documenting and bump version

index.js
@@ -63,7 +63,7 @@ _.extend(NodeMonkey.prototype, {
       suppressOutput: true,
       saveOutput: true,
       silent: false,
-      clientSideStyles: true
+      convertStyles: true
     }, config || {});
 
     this.config.profiler = _.extend({
@@ -102,7 +102,7 @@ _.extend(NodeMonkey.prototype, {
     var stack = (new Error()).stack.toString().split('\n');
     var caller = stack[3]; // First line is just 'Error'
     var callerData = caller.match(/at (.*) \((.*):(.*):(.*)\)/);
-    var config = {clientSideStyles: this.config.clientSideStyles};
+    var config = {convertStyles: this.config.clientSideStyles};
     if(!callerData) callerData = caller.match(/at ()(.*):(.*):(.*)/);
     callerData = {
       callerName: callerData[1],

src/client.js
@@ -1,5 +1,5 @@
 (function() {
-  
+
   //
   // - Global Variabls -
   //
@@ -50,7 +50,7 @@
         }
 
         var toLog;
-        if (data.type == 'log' && data.config && data.config.clientSideStyles && !isSafari) {
+        if (data.type == 'log' && data.config && data.config.convertStyles && !isSafari) {
           toLog = _stylize(data.data, trace);
         } else {
           toLog = data.data;

commit 33b204184feba234c9b9ffd3e03b963344fcb31e

Fix regression introduced with https://github.com/jwarkentin/node-monkey/commit/0bc8402947150c5628d457b5f089234de6d85e30#diff-168726dbe96b3ce427e7fedce31bb0bcR105. The server-side config. property never got updated to match the new name-scheme. Therefore, when the client receives the data, it does not contain the necessary config. property to allow the client to colorize the log.

index.js
@@ -103,7 +103,7 @@ _.extend(NodeMonkey.prototype, {
     var stack = (new Error()).stack.toString().split('\n');
     var caller = stack[3]; // First line is just 'Error'
     var callerData = caller.match(/at (.*) \((.*):(.*):(.*)\)/);
-    var config = {convertStyles: this.config.clientSideStyles};
+    var config = {convertStyles: this.config.convertStyles};
     if(!callerData) callerData = caller.match(/at ()(.*):(.*):(.*)/);
     callerData = {
       callerName: callerData[1],